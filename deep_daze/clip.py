73d5de2e88833c22e67a3d987bd49f5b04982854
Phil Wang <lucidrains@gmail.com>
2022-01-26 09:13:56 -0800

    add larger vision attention net, generously released by OpenAI

--- deep_daze/clip.py
+++ deep_daze/clip.py
@@ -19,6 +19,7 @@ _MODELS = {
     "RN101": "https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt",
     "RN50x4": "https://openaipublic.azureedge.net/clip/models/7e526bd135e493cef0776de27d5f42653e6b4c8bf9e0f653bb11773263205fdd/RN50x4.pt",
     "ViT-B/32": "https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
+    "ViT-L/14": "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt"
 }
 
 def _download(url: str, root: str = os.path.expanduser("~/.cache/clip")):
